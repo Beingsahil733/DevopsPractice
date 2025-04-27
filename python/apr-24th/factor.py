a649858553d02c222b3df3322bee8cf00e975575
Beingsahil733 <beingsahil733@gmail.com>
2025-04-27 22:35:27 +0530

    added changes

---
+++ python/apr-24th/factor.py
@@ -0,0 +1,6 @@
+number = 70
+Factor = number // 2
+while Factor > 1:
+    if number % Factor == 0:
+        print(Factor)
+    Factor = Factor - 1
--- python/compound.py
+++
@@ -1,6 +0,0 @@
-"""This program is to calculate compound interest.
-"""
-
-principle = 290000
-rate_of_interest = 0.05
- 
